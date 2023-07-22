947f7f463b15d42d9d02387b8659a3968ca2167e
Anton <kotlaranton551@gmail.com>
2023-07-22 17:19:07 +0300

    upd

--- OnlineTelegraf/MainWindow.xaml.cs
+++ OnlineTelegraf/MainWindow.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Threading;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using System.Windows.Threading;
 using NAudio;
 using NAudio.Wave;
 
@@ -13,19 +15,72 @@ namespace OnlineTelegraf
     public partial class MainWindow : Window
     {
         WaveOutEvent waveOut;
+        DispatcherTimer timer;
+
+        int tick; int tickToStop = 0;
+        bool timerToStop = false;
 
         public MainWindow()
         {
             InitializeComponent();
-            SetImage("Images/Telegraf.png");
+            MyInitialize();
+            SetImage("Images/Telegraf.png");            
+        }
+
+        void MyInitialize()
+        {
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(1);
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            if(!timerToStop) tick++;
+            else
+            {
+                tickToStop++;
+                if (tickToStop >= tick)
+                {
+                    ForciblyStopSound();
+                    timerToStop = false;
+                }
+            }
         }
 
         void PlaySound()
         {
-            var audioFile = new AudioFileReader("путь_к_вашему_аудиофайлу.mp3");
-            waveOut = new WaveOutEvent();
+            var audioFile = new AudioFileReader("Sounds/Beep.mp3");
+            waveOut = new WaveOutEvent(); // Громкость от 0.0 до 1.0
             waveOut.Init(audioFile);
+            waveOut.Volume = (float)tbVolume.Value / 10;
             waveOut.Play();
+
+            timer.Start();
+        }  
+
+        void StopSound()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
+
+            if (tick < 10) tick = 10;
+            lblTIck.Content = tick.ToString();
+            timer.Stop();
+        } 
+        
+        void ForciblyStopSound()
+        {
+            if (waveOut != null)
+            {
+                waveOut.Stop();
+                waveOut.Dispose();
+                waveOut = null;
+            }
         }
 
         void SetImage(string imagePath)
@@ -46,20 +101,33 @@ namespace OnlineTelegraf
             }
         }
 
-        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e) { }
+        private void Window_PreviewKeyUp(object sender, KeyEventArgs e) { }
+
+        private void Window_PreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
         {
-            if (e.Key == Key.Space)
-            {
-                SetImage("Images/TelegrafBeep.png");
-            }
+            SetImage("Images/TelegrafBeep.png");
+
+            tick = 0;
+            PlaySound();
         }
 
-        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
+        private void Window_PreviewMouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
-            if (e.Key == Key.Space)
-            {
-                SetImage("Images/Telegraf.png");
-            }
+            SetImage("Images/Telegraf.png");
+
+            StopSound();
+
+            //Thread.Sleep(100);
+
+            //timerToStop = true;
+            //tickToStop = 0;
+            //PlaySound();
+        }
+
+        private void btnGiveControl_Click(object sender, RoutedEventArgs e)
+        {
+            btnGiveControl.IsEnabled = false;
         }
     }
 }
